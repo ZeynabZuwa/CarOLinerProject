580cf294cec5b47e5c0b7f63b3172167acdbe743
ZeynabZuwa <zayzuwa@gmail.com>
2022-05-23 15:42:21 +0200

    started on seeding service

--- CarOLiner.Services/Features/Seeding/SeedService.cs
+++ CarOLiner.Services/Features/Seeding/SeedService.cs
@@ -1,4 +1,5 @@
 ﻿using CarOLiner.Shared.IRepositories;
+using CarOLiner.Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,42 @@ namespace CarOLiner.Services.Features.Seeding
     public class SeedService : ISeedService
     {
         private readonly IProductRepository _productRepository;
+
+        public SeedService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task SeedDatabaseAsync()
+        {
+            var Dress = new Product()
+            {
+                ProductId = Guid.NewGuid(),
+                ProductName = "Dress",
+                Items = new List<Item>()
+            };
+            var DressItem = new List<Item>();
+
+            var DressItemRed = new Item()
+            {
+                ItemId = Guid.NewGuid(),
+                ColorName = "Red",
+                ProductId = Dress.ProductId
+            };
+
+            var DressItemRedVariants = new List<Variant>()
+            {
+                new Variant()
+                {
+                    VariantId = Guid.NewGuid(),
+                    Size = "S",
+                    ItemId = 
+
+                }
+            }
+            
+
+
+        }
     }
 }
