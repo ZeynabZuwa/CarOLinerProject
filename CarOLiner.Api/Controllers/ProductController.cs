e43b01a79af41ce7b6b70f9cc562cfeca123682e
ZeynabZuwa <zayzuwa@gmail.com>
2022-05-24 22:47:22 +0200

    Added Get, Put and delete to ProductController

--- CarOLiner.Api/Controllers/ProductController.cs
+++ CarOLiner.Api/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using CarOLiner.Services.Features.Products;
+using CarOLiner.Shared.RequestModels;
 using CarOLiner.Shared.ResponseModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,8 +20,23 @@ namespace CarOLiner.Api.Controllers
         [HttpGet]
         public async Task<ActionResult<List<ProductResponse>>> GetAllProducts()
         {
-            // TODO: Return all products to a view
-            var productResponses = await _productService.GetAllProducts();
+            // TODO: Return all products
+            var productResponse = await _productService.GetAllProducts();
+            if (productResponse != null && productResponse.Count > 0)
+            {
+                return Ok(productResponse);
+            }
+            else
+            {
+                return BadRequest(productResponse);
+            }
+        }
+
+        [HttpGet("{}")]
+        public async Task<ActionResult<ProductResponse>> GetProductById(Guid productId)
+        {
+            // TODO: Return one specific product with Guid
+            var productResponses = await _productService.GetProductById(productId);
             if (productResponses != null)
             {
                 return Ok(productResponses);
@@ -30,5 +46,37 @@ namespace CarOLiner.Api.Controllers
                 return BadRequest(productResponses);
             }
         }
+
+        [HttpPut("{}")]
+        public async Task<ActionResult> UpdateProduct(ProductRequest productRequest)
+        {
+            // TODO: Delete one specific product with Guid
+            var productResponse = await _productService.UpdateProduct(productRequest);
+            if (productResponse != null)
+            {
+                return Ok(productResponse);
+            }
+            else
+            {
+                return BadRequest(productResponse);
+            }
+        }
+
+        [HttpDelete("{productId}")]
+        public async Task<ActionResult> DeleteProductById(Guid productId)
+        {
+            // TODO: Delete one specific product with Guid
+            var productResponses = await _productService.GetProductById(productId);
+            if (productResponses != null)
+            {
+                return Ok(productResponses);
+            }
+            else
+            {
+                return BadRequest(productResponses);
+            }
+        }
+
+    
     }
 }
--- CarOLiner.Services/Features/Products/IProductService.cs
+++ CarOLiner.Services/Features/Products/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using CarOLiner.Shared.ResponseModels;
+﻿using CarOLiner.Shared.RequestModels;
+using CarOLiner.Shared.ResponseModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,8 @@ namespace CarOLiner.Services.Features.Products
     public interface IProductService
     {
         Task<List<ProductResponse>> GetAllProducts();
+        Task<ProductResponse> GetProductById(Guid productId);
+        Task<ProductResponse> DeleteProduct(Guid productId);
+        Task<ProductResponse> UpdateProduct(ProductRequest productRequest);
     }
 }
